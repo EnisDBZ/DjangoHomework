548bc132712926619da2496e7b3dc7e56f5cedb9
Enis Doğanbenzer <enisdoganbenzer@hotmail.com>
2025-03-04 08:43:28 +0300

    cart,add_to_cart and remove_from_cart urls added.

--- website_app/urls.py
+++ website_app/urls.py
@@ -4,10 +4,13 @@ from . import views
 app_name = 'website_app'
 
 urlpatterns = [
-    path('home/', views.index,name ='index'),
+    path('', views.index,name ='index'),
     path('login/',views.login_view, name = 'login'),
     path('signup/', views.signup_view, name='signup'),
     path('logout/',views.LogoutView, name='logout'),
     path('success/', views.success_view, name='success'),
     path('products/', views.products_view, name='products'),
+    path('cart/',views.cart_view,name="cart"),
+    path('add/<int:product_id>/',views.add_to_cart,name="add_to_cart"),
+    path('remove/<int:item_id>/', views.remove_from_cart, name = "remove_from_cart"),
 ]
