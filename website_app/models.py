191d9c19932ebefaf52674b3809aa824a48cd8eb
Enis Doğanbenzer <enisdoganbenzer@hotmail.com>
2025-03-04 08:42:35 +0300

    Product and CartItem models created.

--- website_app/models.py
+++ website_app/models.py
@@ -10,3 +10,23 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username  # Kullanıcı adını döndür
+    
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100)
+    product_description = models.TextField(null=True)
+    product_price = models.DecimalField(max_digits=6,decimal_places=2)
+    product_image = models.ImageField(upload_to='products/')
+
+    def __str__(self):
+        return self.product_name
+    
+
+
+class CartItem(models.Model):
+    cart_product_name = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart_quantity = models.PositiveIntegerField(default=0)
+    user = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.cart_quantity} x {self.cart_product_name.product_name}"
