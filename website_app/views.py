31841f712b130f94cfd39c26d071f765fffe1866
Enis Doğanbenzer <enisdoganbenzer@hotmail.com>
2025-03-04 08:45:07 +0300

    Shopping cart functions added.Separete logins for staff and normal user added.

--- website_app/views.py
+++ website_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth import authenticate, login,logout,get_user_model
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 import time
+from .models import Product,CustomUser,CartItem
 
 # Create your views here.
 
@@ -12,24 +13,50 @@ def index(request):
     return render(request,'website_app/index.html')
 
 def success_view(request):
-    return render(request,'website_app/scces.html')
+    pass
 
+
+# !--- Shopping Cart functions
 def products_view(request):
-    return render(request,'website_app/products.html')
-        
+    products = Product.objects.all()
+    return render(request,'website_app/products.html',{'products':products})
 
+
+def cart_view(request):
+    cart_items = CartItem.objects.filter(user = request.user)
+    total_price = sum(item.cart_product_name.product_price * item.cart_quantity for item in cart_items)
+    return render(request,'website_app/cart.html',{'cart_items':cart_items,'total_price':total_price})
+
+def add_to_cart(request,product_id):
+    product = Product.objects.get(id=product_id)
+    cart_item, created = CartItem.objects.get_or_create(cart_product_name = product,user = request.user)
+    cart_item.cart_quantity += 1
+    cart_item.save()
+    return redirect('website_app:products')
+
+def remove_from_cart(request,item_id):
+    cart_item = CartItem.objects.get(id = item_id)
+    cart_item.delete()
+    return redirect('website_app:cart')
+# Shopping Cart codes ends here !---
+User =get_user_model()
 def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
 
+
+
         if user is not  None:
-            login(request, user)
-            return redirect('website_app:success')  # Redirect to a success page
+            login(request,user)
+            if user.is_staff or user.is_superuser:
+                login(request, user)
+                return redirect('/admin/')  # Redirect to a success page
+        
+            return render(request,'website_app/index.html')
         else:
-            return render(request, 'registration/login.html', {'error': 'Invalid credentials'})
-    
+            return render(request,'registration/login.html',{'error':'Invalid credentials'})
     # Render the login page for GET requests
     return render(request, 'registration/login.html')
 
