2bd16ab080650c8cbf03994e68473555997ef7ad
Enis Doğanbenzer <enisdoganbenzer@hotmail.com>
2025-03-05 08:53:04 +0300

    redirect_url added

--- website_app/urls.py
+++ website_app/urls.py
@@ -5,6 +5,7 @@ app_name = 'website_app'
 
 urlpatterns = [
     path('', views.index,name ='index'),
+    path('',views.redirect_url_,name="redirect_url"),
     path('login/',views.login_view, name = 'login'),
     path('signup/', views.signup_view, name='signup'),
     path('logout/',views.LogoutView, name='logout'),
@@ -13,4 +14,5 @@ urlpatterns = [
     path('cart/',views.cart_view,name="cart"),
     path('add/<int:product_id>/',views.add_to_cart,name="add_to_cart"),
     path('remove/<int:item_id>/', views.remove_from_cart, name = "remove_from_cart"),
+    
 ]
